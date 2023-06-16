d71681ef74a153352f43395ff703b9fea661f873
er-0 <95697943+er-0@users.noreply.github.com>
2023-06-16 21:59:08 +0300

    up to 6.8

---
+++ redux-anecdotes/readme.md
@@ -0,0 +1,1 @@
+base of the application from https://github.com/fullstack-hy2020/redux-anecdotes
---
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { vote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const addVote = (id) => {
+    console.log('vote', id)
+    dispatch(vote(id))
+  }
+
+  return (
+    <div>
+    {anecdotes
+      .sort((a, b) => b.votes - a.votes)
+      .map(anecdote =>
+      <div key={anecdote.id}>
+        <div>
+          {anecdote.content}
+        </div>
+        <div>
+          has {anecdote.votes} votes
+          <button onClick={() => addVote(anecdote.id)}>vote</button>
+        </div>
+      </div>
+    )}
+    </div>
+  )
+}
+
+export default AnecdoteList
