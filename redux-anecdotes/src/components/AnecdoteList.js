1a1091818da4d468cd0c934431c68ab0cc90f78a
er-0 <95697943+er-0@users.noreply.github.com>
2023-06-20 15:22:13 +0300

    6.9 (ennen redux toolkitin käyttöönottoa)

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -2,9 +2,16 @@ import { useSelector, useDispatch } from 'react-redux'
 import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state)
+  
+  const filter = useSelector(state => state.filter)
+  const anecdotes = useSelector(state => {
+    if (state.filter === '') {
+      return state.anecdotes
+    }
+    return state.anecdotes.filter(a => a.content.includes(filter))
+  })
   const dispatch = useDispatch()
-
+  console.log(anecdotes, 'anek')
   const addVote = (id) => {
     console.log('vote', id)
     dispatch(vote(id))
